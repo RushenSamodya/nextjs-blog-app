4f769bedcdcf9d15a04aed168cfb75136cc226e2
RushenSamodya <114132773+RushenSamodya@users.noreply.github.com>
2024-02-07 22:30:43 +0530

    added components

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./links.module.css";
+
+const Links = () => {
+  const links = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+  return (
+    <div className={styles.links}>
+      {links.map((link) => (
+        <Link href={link.path} key={link.title}>
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Links;
